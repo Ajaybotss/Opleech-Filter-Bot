bf70004894de2aef275618f9dce5ff11e905ee33
Ajaybotss <kallaguntaajay246@gmail.com>
2024-08-25 02:08:59 +0530

    Update info.py

--- info.py
+++ info.py
@@ -112,9 +112,9 @@ else:
     ON_HEROKU = False
 HAS_SSL=bool(getenv('HAS_SSL',False))
 if HAS_SSL:
-    URL = "https://opleech-filter-bot-fg4c.onrender.com/".format(FQDN)
+    URL = "https://opleech-filter-bot-qn5x.onrender.com".format(FQDN)
 else:
-    URL = "https://opleech-filter-bot-fg4c.onrender.com/".format(FQDN)
+    URL = "https://opleech-filter-bot-qn5x.onrender.com".format(FQDN)
 
 
 
